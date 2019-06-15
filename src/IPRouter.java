d9e554deb8df58727a50a5d12520e561f2bb6422
Jay Van Nostrand <46982944+Jvanvan29@users.noreply.github.com>
2019-06-15 15:58:57 -0400

    Update IPRouter.java

--- src/IPRouter.java
+++ src/IPRouter.java
@@ -6,13 +6,15 @@ public class IPRouter {
 	private int mask;
 	private Map <String, String> rTable;
 	private int [] octets;
+        private String [] binCon;//For array of each octet coverted to binary
 	
 	public IPRouter(String inAdd, Map<String,String> inRTable)
 	{
 		address = inAdd.substring(0, inAdd.length()-(inAdd.length()-inAdd.indexOf("/")));
 		mask = Integer.parseInt(inAdd.substring(inAdd.length()-(inAdd.length()-inAdd.indexOf("/")-1)));
 		rTable = inRTable;
-		octets = new int [4];		
+		octets = new int [4];	
+   
 	}
 	
 	//Utilizes setMask and setOctets because those things should only change when a new address
@@ -34,6 +36,12 @@ public class IPRouter {
 	}
 
 	
+        public int[] getOctets(){
+            
+            return octets;
+            
+        }
+        
 	private void setMask(String inAdd)
 	{
 		mask = Integer.parseInt(inAdd.substring(inAdd.length()-(inAdd.length()-inAdd.indexOf("/")-1)));
@@ -46,6 +54,52 @@ public class IPRouter {
 		return result;
 	}
 	
-
-	
+        //Convert IP address to binary 
+        public void convertOctetsToBinary(int[] old){
+            
+            binCon = new String[4];//For convert to binary
+            
+            for(int i = 0; i< old.length; i++){
+                
+               binCon[i] = changeToBinary(old[i])+"";
+               
+               //System.out.println(binCon[i]);- check to see it works
+                
+            }//End for loop for iterating through binary array
+               
+        }//End convertOctetsToBinary
+        
+        public String changeToBinary(int octet){
+            
+            String temp = "";
+            String bin = "";
+            boolean cont = true;
+            
+           //Use while loop to make a sequence of binary digits 
+           while(cont){
+               
+            if((octet%2 == 0 || octet%2 == 1) && octet != 0){
+               
+                 temp += (octet%2+"");
+                 octet=octet/2;
+               
+            }else{
+               
+                 cont=false;
+               
+            }//End logic chain
+            
+           }//End while
+           
+           //Reverse binary digits to accurately represent binary conversion
+           for(int i = temp.length()-1; i >= 0; i--){
+               
+               bin+= temp.charAt(i)+"";
+               
+           }//End reverse string for loop
+           
+           return bin;
+            
+        }//End changeToBinary
+        
 }
